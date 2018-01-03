3ce5fb339190b45b64a5edf49f9183c5de365c23
VictoriaVasys <vvasys@gmail.com>
2018-01-03 11:33:42 -0700

    Add total veggie count

--- lib/garden.rb
+++ lib/garden.rb
@@ -1,9 +1,13 @@
 class Garden
   
-  attr_reader :rows
+  attr_reader :veggie_rows
   
   def initialize(veggie_rows={})
-    @rows = veggie_rows
+    @veggie_rows = veggie_rows
   end
-  
+
+  def total_veggies
+    veggie_rows.values.sum
+  end
+
 end
--- test/garden_test.rb
+++ test/garden_test.rb
@@ -6,14 +6,20 @@ class GardenTest < Minitest::Test
   
   def test_it_exists
     garden = Garden.new
-    
+
     assert_instance_of Garden, garden
   end
   
   def test_it_has_rows
     garden = Garden.new({carrots: 12, beets: 20})
+
+    assert_equal 12, garden.veggie_rows[:carrots]
+  end
+  
+  def test_total_veggies_calculates_total_number_of_veggies
+    garden = Garden.new({carrots: 12, beets: 20})
     
-    assert_equal 12, garden.rows[:carrots]
+    assert_equal 32, garden.total_veggies
   end
   
 end
